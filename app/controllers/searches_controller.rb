352e3f38d1d792c1e757be7b477884f0786edefc
KentaMatsu <matsuken102@gmail.com>
2021-08-07 06:23:35 +0000

    finish_search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,14 @@
 class SearchesController < ApplicationController
 
-  def serch
-    @model = params["model"]
+  def search
+    @model = params["search"]["model"]
     #選択したmodelを@modelに代入
-    @content = params["content"]
+    @content = params["search"]["content"]
     #検索にかけた文字列(ここではcontent)を@contentに代入
-    @method = params["method"]
-    #選択した検索方法howを@howに代入
+    @method = params["search"]["method"]
+    #選択した検索方法methodを@methodに代入
     @datas = search_for(@method, @model, @content)
+    #binding.pry
     #search_forの引数にインスタンス変数を定義
     #@datasに最終的な検索結果が入る
   end
@@ -23,15 +24,15 @@ class SearchesController < ApplicationController
       elsif
         method == 'forward'
         # 選択した検索方法がが前方一致だったら
-        User.where('name LIKE ?', "#{content}%")
+        User.where('name LIKE ?', content+'%')
       elsif
         method == 'backward'
         # 選択した検索方法がが後方一致だったら
-        User.where('name LIKE ?', "%#{content}")
+        User.where('name LIKE ?', '%'+content)
       elsif
         method == 'partical'
         # 選択した検索方法がが部分一致だったら
-        User.where("name LIKE ?", "%#{content}%")
+        User.where("name LIKE ?", '%'+content+'%')
       end
     elsif model == 'book'
     # 選択したモデルがbookだったら
@@ -39,15 +40,15 @@ class SearchesController < ApplicationController
         Book.where(title: content)
       elsif
         method == 'forward'
-        Book.where('title LIKE ?', "#{content}%")
+        Book.where('title LIKE ?', content+'%')
         #前方一致→モデル名.where("カラム名 LIKE ?", "値%")
       elsif
         method == 'backward'
-        Book.where('title LIKE ?', "%#{content}")
+        Book.where('title LIKE ?', '%'+content)
         #後方一致→モデル名.where("カラム名 LIKE ?", "%値")
       elsif
         method == 'partical'
-        Book.where("title LIKE ?", "%#{content}%")
+        Book.where("title LIKE ?", '%'+content+'%')
         #部分一致→モデル名.where("カラム名 LIKE ?", "%値%")
       end
     end
