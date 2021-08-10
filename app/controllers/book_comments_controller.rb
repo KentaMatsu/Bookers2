d55fd0a9ab2c865d5db03f979d01b5f10ea620c7
KentaMatsu <matsuken102@gmail.com>
2021-08-10 05:22:58 +0000

    finish_Asynchronous

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,20 +1,23 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
-    book = Book.find(params[:book_id])
-    book_comment = BookComment.new(book_comment_params)
-    book_comment.user_id = current_user.id
-    book_comment.book_id = book.id
-    if book_comment.save
-      redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
+    if @book_comment.save
+      #redirect_back(fallback_location: root_path)
     else
+      @user = @book.user
       render 'books/show'
     end
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @book = Book.find(params[:book_id])
+    #redirect_to book_path(params[:book_id])
   end
 
   private
